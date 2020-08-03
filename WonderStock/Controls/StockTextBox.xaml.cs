4a7bebe66c910ddb2fe2e7800b4c8abc5fc86b8c
daehyun kang <soejdtodxn@kw.ac.kr>
2020-08-04 01:01:35 +0900

    Set Skeleton UI

---
+++ WonderStock/Controls/StockTextBox.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WonderStock.Controls
+{
+    public partial class StockTextBox : UserControl
+    {
+        public StockTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
