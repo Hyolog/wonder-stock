18f4afee9999d0e10830cec473315b7ec5a69b01
daehyun kang <soejdtodxn@kw.ac.kr>
2020-08-15 01:26:18 +0900

    Add UnhandledException handling & init StockManager

--- WonderStock/App.xaml.cs
+++ WonderStock/App.xaml.cs
@@ -1,8 +1,58 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using NLog;
 
 namespace WonderStock
 {
     public partial class App : Application
     {
+        private Logger logger = LogManager.GetCurrentClassLogger();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            InitUnhandledException();
+
+            StockManager.Init();
+        }
+
+        private void InitUnhandledException()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            LoggingUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                LoggingUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+                e.Handled = true;
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                LoggingUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+                e.SetObserved();
+            };
+        }
+
+        private void LoggingUnhandledException(Exception exception, string source)
+        {
+            string message = $"Unhandled exception ({source})";
+
+            try
+            {
+                System.Reflection.AssemblyName assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName();
+                message = string.Format("Unhandled exception in {0} v{1}", assemblyName.Name, assemblyName.Version);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Exception in LoggingUnhandledException");
+            }
+            finally
+            {
+                logger.Error(exception, message);
+            }
+        }
     }
 }
